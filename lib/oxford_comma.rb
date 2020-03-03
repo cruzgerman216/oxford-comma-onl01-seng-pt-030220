f96acc4bfdffe7eabe0967290c8a1d594f935c8e
German Cruz <cruzgerman216@yahoo.com>
2020-03-03 20:42:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,11 @@ def oxford_comma(array)
   if array.size == 1
     str = array[array.size - 1]
   else
+    str2 = array[array.size-1]
     arr = array
      array.pop
     str = array.join(", ")
-    str = str +" and #{arr[arr.size - 1]}"
+    str = str +" and #{str2}"
   end
 
   return str
