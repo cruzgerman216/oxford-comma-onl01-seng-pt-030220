ee9bbc003d16569829986c5a6178d5ac8e02a278
German Cruz <cruzgerman216@yahoo.com>
2020-03-03 20:40:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   arr = array
    array.pop
-  str = array.join(",")
-  str = str +" and #{arr[arr.size - 1]}"
+  str = array.join(", ")
+  str = str +"and #{arr[arr.size - 1]}"
   return str
 end
 
