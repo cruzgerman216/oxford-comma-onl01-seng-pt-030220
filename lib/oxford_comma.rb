44fd509d05421442e7e143173618d1b4971a6611
German Cruz <cruzgerman216@yahoo.com>
2020-03-03 20:38:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-  arr = array.pop
-  return [1,2,3]
+  arr = array.pop()
+  str = arr.join(",")
+  str = " and #{array[array.length -1]}"
+  return str
 end
 
 puts oxford_comma(["1","2","3"])
